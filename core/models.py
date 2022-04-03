220431dbace40582b1910e3c52c22b63ebc06dd8
bugbytes-io <bugbytes.io@gmail.com>
2022-04-03 18:16:12 +0100

    Added final code

--- core/models.py
+++ core/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+class Comment(models.Model):
+    text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    post_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.text
--- core/views.py
+++ core/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from core.models import Comment
+from core.spam_model import predict_spam
 
 # Create your views here.
+def comments(request):
+    comments = Comment.objects.all()
+    context = {'comments': comments}
+    return render(request, 'comments.html', context)
+
+
+def check_spam(request):
+    comments = Comment.objects.all()
+    predictions = predict_spam(comments.values_list('text', flat=True))
+    
+    # zip up the comments and predictions
+    context = {'comments': zip(comments, predictions)}
+    return render(request, 'partials/comments-spam.html', context)
+
+
+def delete_comment(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.delete()
+    return HttpResponse(status=204)
